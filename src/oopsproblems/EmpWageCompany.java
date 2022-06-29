23aa7814b4fada0ba8cdb301dc6d21f7a3cfaeaa
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 04:34:22 +0530

    Added PartTime Employee

--- src/oopsproblems/EmpWageCompany.java
+++ src/oopsproblems/EmpWageCompany.java
@@ -6,13 +6,18 @@ public class EmpWageCompany {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to Employee Wage Computation");
 		int employeePresent = 1;
+		int employeePartTime = 2;
 		int WagePerHour = 20;
 		int numberOfHours = 0;
 		int empWagePerDay = 0;
-		int isPresent = (int) Math.floor(Math.random()*10) %2;
+		int isPresent = (int) Math.floor(Math.random()*10) %3;
 		if (isPresent == employeePresent) {
 			numberOfHours =8;
 			System.out.println("Employee is Present");
+		}
+		else if (isPresent == employeePartTime){
+			numberOfHours =4;
+			System.out.println("Employee is Part Time ");
 		}
 		else {
 			numberOfHours = 0;
@@ -20,7 +25,6 @@ public class EmpWageCompany {
 		}
 		empWagePerDay = numberOfHours * WagePerHour;
 		System.out.println("Employee Wage daily =" + empWagePerDay);
-		
 		
 	}
 
