076830fbe0b1aa1aba7621e4c9387612e25fd41e
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 05:00:00 +0530

    Compute Employee Wage with Class method

--- src/oopsproblems/EmpWageCompany.java
+++ src/oopsproblems/EmpWageCompany.java
@@ -2,54 +2,76 @@ package oopsproblems;
 
 public class EmpWageCompany {
 
+	/**
+	 * Created Separate Class named EmpWageObject
+	 * Assigned some instance variables with public static final
+	 * because we are going to use them in methods
+	 * Created a Function Welcome with print statement
+	 * created another function Variables and Assigned Values
+	 *  Created another function Method and did all the computation Required.
+	 *  For Computation, Took a WHILE loop and a Random Variable
+	 *  A SWITCH CASE created for separating Present or absent or PartTime
+	 *  Finally Computed TotalEmployee of Wage of Full Month and for Max Working Hours
+	 *  
+	 * 
+	 */
+	
 	public static final int employeePresent = 1;
 	public static final int employeePartTime = 2;
+	
 	public static final int WagePerHour = 20;
+	
 	public static final int NumberOfWorkingDays =20;
 	public static final int TotalHoursinMonth = 100;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stu
-		System.out.println("Welcome to Employee Wage Computation Program");
 	
-		int numberOfHours = 0;
-		int empWagePerDay = 0,nHours=0;
-		int totalWorkDays = 0 , totalWorkHours = 0;
-		int totalEmployeeWage =0;
-		while (totalWorkHours< TotalHoursinMonth && totalWorkDays < NumberOfWorkingDays) {
-		//totalWorkDays++;
-		int isPresent = (int) Math.floor(Math.random()*10) %3;
-		switch (isPresent) {
-			case employeePresent :
-				numberOfHours =8;
-				totalWorkHours =totalWorkHours + numberOfHours;
-				System.out.println("Employee is Prsent");
-				System.out.println("Employee earn = " + numberOfHours * WagePerHour);
-				totalWorkDays++;
-				nHours+=numberOfHours;
-				break;
-			case employeePartTime :
-				numberOfHours =4;
-				totalWorkHours =totalWorkHours + numberOfHours;
-				System.out.println("Employee is Part Time Present");
-				System.out.println("Employee earn with PartTime= " + numberOfHours* WagePerHour);
-				totalWorkDays++;
-				nHours+=numberOfHours;
-				
-				break;
-
-			default :
-				numberOfHours = 0;
-				totalWorkHours =totalWorkHours + numberOfHours;
-				System.out.println("Employee is not Present");
-				System.out.println("Employee earn = 0");
-				break;
-				}
+	 static int  numberOfHours;
+	 static int empWagePerDay;
+	 static int nHours;
+     static int totalWorkDays;
+     static int totalWorkHours;
+	 static int totalEmployeeWage;
+	 
+	//Welcome Message
+	 public static void  Welcome() {
+		System.out.println("Welcome to Employee Wage Computation program");
 		}
-		System.out.println("days  = " + totalWorkDays + " No. of Hours = " +nHours);
-		System.out.println(	"Employee Monthly Earn = " + ((numberOfHours) * WagePerHour) * NumberOfWorkingDays);
-		totalEmployeeWage= totalWorkHours * WagePerHour;
-		System.out.println("Total Employee Wage is "+ totalEmployeeWage);
-	}
+	 //Variables
+	public static void Variables () {
+		 numberOfHours = 0;
+		 empWagePerDay = 0;
+		 nHours=0;
+	     totalWorkDays = 0;
+	     totalWorkHours = 0;
+		 totalEmployeeWage =0;
+	 	}
+	// Method
+	 public static void Method() {
+		while (totalWorkHours< TotalHoursinMonth && totalWorkDays <= NumberOfWorkingDays) {
+			int isPresent = (int) Math.floor(Math.random()*10) %3;
+			switch (isPresent) {
+				case employeePresent :
+					numberOfHours =8;
+					totalWorkHours =totalWorkHours + numberOfHours;
+					totalWorkDays++;
+					nHours+=numberOfHours;
+					break;
+				case employeePartTime :
+					numberOfHours =4;
+					totalWorkHours =totalWorkHours + numberOfHours;
+					totalWorkDays++;
+					nHours+=numberOfHours;
+					break;
+
+				default :
+					numberOfHours = 0;
+					totalWorkHours =totalWorkHours + numberOfHours;
+					break;
+					}
+			}
+			System.out.println("For a Month Max Days = " + totalWorkDays + " No. of Hours Reached = " +nHours);
+			System.out.println(	"Employee If Worked Monthly Earn = " + ((numberOfHours) * WagePerHour) * NumberOfWorkingDays);
+			totalEmployeeWage= totalWorkHours * WagePerHour;
+			System.out.println("Total Employee Wage for Max Hours or Days reached is "+ totalEmployeeWage);
+		}
 
 }
---
+++ src/oopsproblems/EmpWageCompanyMain.java
@@ -0,0 +1,28 @@
+package oopsproblems;
+
+/**
+ * 
+ * Employee Wage using OOPS Concept
+ * @author singa
+ *
+ */
+public class EmpWageCompanyMain {
+	/*
+	 * Created a Class EmpWageMain.
+	 * Created a function Main
+	 * Created a New Object EmpWageObj.
+	 * Call the Required Functions using new Object name.
+	 * Here, welcome and Method Functions were Called Directly
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmpWageCompany EmpWageObj = new EmpWageCompany();
+		EmpWageObj.Welcome();
+		EmpWageObj.Variables();
+		EmpWageObj.Method();
+
+	}
+
+}
