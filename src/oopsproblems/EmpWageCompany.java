e13c3e39d164b07d59c38d1159eedd5e2e77b6db
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 04:36:55 +0530

    Using Switch Case Replacing IF

--- src/oopsproblems/EmpWageCompany.java
+++ src/oopsproblems/EmpWageCompany.java
@@ -2,30 +2,33 @@ package oopsproblems;
 
 public class EmpWageCompany {
 
+	public static final int employeePresent = 1;
+	public static final int employeePartTime = 2;
+	public static final int WagePerHour = 20;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to Employee Wage Computation");
-		int employeePresent = 1;
-		int employeePartTime = 2;
-		int WagePerHour = 20;
+		// TODO Auto-generated method stu
+		System.out.println("Welcome to Employee Wage Computation Program");
+	
 		int numberOfHours = 0;
 		int empWagePerDay = 0;
 		int isPresent = (int) Math.floor(Math.random()*10) %3;
-		if (isPresent == employeePresent) {
-			numberOfHours =8;
-			System.out.println("Employee is Present");
-		}
-		else if (isPresent == employeePartTime){
-			numberOfHours =4;
-			System.out.println("Employee is Part Time ");
-		}
-		else {
-			numberOfHours = 0;
-		System.out.println("Employee is Absent");
+		switch (isPresent) {
+			case employeePresent :
+				numberOfHours =8;
+				System.out.println("Employee is Present");
+				break;
+			case employeePartTime :
+				numberOfHours =4;
+				System.out.println("Employee is Part Time ");
+				break;
+			default :
+				numberOfHours = 0;
+				System.out.println("Employee is Absent");
+		
 		}
 		empWagePerDay = numberOfHours * WagePerHour;
 		System.out.println("Employee Wage daily =" + empWagePerDay);
-		
 	}
 
 }
