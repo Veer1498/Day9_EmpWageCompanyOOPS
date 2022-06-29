603f1bdd2ef025ccce303abc7bda778b759a0376
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 04:49:32 +0530

    Monthly Wage of Employee

--- src/oopsproblems/EmpWageCompany.java
+++ src/oopsproblems/EmpWageCompany.java
@@ -5,6 +5,7 @@ public class EmpWageCompany {
 	public static final int employeePresent = 1;
 	public static final int employeePartTime = 2;
 	public static final int WagePerHour = 20;
+	public static final int NumberOfWorkingDays =20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
@@ -12,6 +13,8 @@ public class EmpWageCompany {
 	
 		int numberOfHours = 0;
 		int empWagePerDay = 0;
+		int i, totalEmployeeWage =0;
+		for (i=0; i<NumberOfWorkingDays; i++) {
 		int isPresent = (int) Math.floor(Math.random()*10) %3;
 		switch (isPresent) {
 			case employeePresent :
@@ -25,10 +28,13 @@ public class EmpWageCompany {
 			default :
 				numberOfHours = 0;
 				System.out.println("Employee is Absent");
-		
+				break;
 		}
 		empWagePerDay = numberOfHours * WagePerHour;
+		totalEmployeeWage= totalEmployeeWage + empWagePerDay;
 		System.out.println("Employee Wage daily =" + empWagePerDay);
+		}	
+		System.out.println("Total Employee Wage is "+ totalEmployeeWage);
 	}
 
 }
